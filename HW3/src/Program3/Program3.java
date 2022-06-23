29b015b6b70a06d6c3222d03707c8ea5a86a355e
jpljason <jpljason24@gmail.com>
2022-06-23 19:46:31 -0400

    Updated version of program3

--- HW3/src/Program3/Program3.java
+++ HW3/src/Program3/Program3.java
@@ -4,50 +4,67 @@ import java.io.*;
 import java.text.*;
 import java.time.*;
 /*
-This program reads in data from another file in the form of record dates. It compares these dates to the current date using various computations and prints the age of all
-the records in the form of years,months and days.
+This program takes data from a file in the form of record dates as input. It then compares these dates to the current date with various computations
+and finally prints the age of the record in the form of a message
 */
 public class Program3 {
 
 	public static void main(String[] args) throws Exception{
-		int currentYear, currentDay, currentMonth;	//Variables to represent date of current date
+		String recordDate;	//Variable used to assign value of date in file
+		int currentYear, currentDay, currentMonth, recordYear, recordMonth, recordDay;	//Variables to represent date of current date
 		File inputfile = new File("inputfile.txt");	//File to be scanned for data
 		Scanner input = new Scanner(inputfile);
 		String [] dateArray = findCurrentDate();	//Find the current date and insert values
-	  currentYear = Integer.parseInt(dateArray[0]);	//Assigning following variables according to their value of either year, month or day in the form of integers
-	  currentMonth = Integer.parseInt(dateArray[1]);
-	  currentDay = Integer.parseInt(dateArray[2]);
-	  findAgeDifference(currentYear, currentMonth, currentDay, input);	//Find the age difference and print message
-	  input.close();
-	}
-	//Method to find the age difference of the record in years, months and days
-	public static void findAgeDifference(int currentYear, int currentMonth, int currentDay, Scanner input) {
-		String recordDate;	//Variable to read in dates from file
+		currentYear = Integer.parseInt(dateArray[0]);	//Assigning following variables according to their value of either year, month or day in the form of integer
+		currentMonth = Integer.parseInt(dateArray[1]);
+		currentDay = Integer.parseInt(dateArray[2]);
 		while (input.hasNext()) {
-	    	input.next();	//Skip first word in the file
-	    	recordDate = input.next();
-	    	String [] array = recordDate.split("/");	//Split the date by year, month and day
-	    	int recordYear, recordDay, recordMonth;		//Variables to represent the date of record
-	    	recordYear = Integer.parseInt(array[0]);	//Converting elements to integers
-	    	recordMonth = Integer.parseInt(array[1]);
-	    	recordDay = Integer.parseInt(array[2]);
-	    	LocalDate record_Date = LocalDate.of(recordYear, recordMonth, recordDay);	//Create LocalDate object to assign current date and record's date
-	    	LocalDate currentDate = LocalDate.of(currentYear, currentMonth, currentDay);
-	    	Period age = Period.between(record_Date, currentDate);	//Creating a Period object to find the time between the record date and current date
-	    	if (age.getYears()<0 || age.getMonths()<0 || age.getDays()<0)	//If the record's date is ahead of the current date, it is an invalid date
-	    		System.out.print("Invalid record date");
-	    	else
-	    		System.out.printf("The age of the record is %d years, %d months, and %d days old."	//If it's a valid date, print the age of the record
-	    				, age.getYears(),age.getMonths(),age.getDays());
-	    	System.out.println();	//skip a line
+			input.next();	//Skip first word in file
+			recordDate = input.next();
+	    String [] array = recordDate.split("/");	//Split date into appropriate values
+	    recordYear = Integer.parseInt(array[0]);	//Converting elements to integers
+	    recordMonth = Integer.parseInt(array[1]);
+	    recordDay = Integer.parseInt(array[2]);
+	    findAgeDifference(currentYear, currentMonth, currentDay, recordYear, recordMonth, recordDay);	//Find age of record
 	    }
+	    input.close();
+	}
+	//This method takes the dates of the record and the current date and compares them to find the age of the record. It prints a message with this age
+	public static void findAgeDifference(int currentYear, int currentMonth, int currentDay, int recordYear, int recordMonth, int recordDay) {
+		YearMonth daysInMonth = YearMonth.of(recordYear, recordMonth);
+		int days = daysInMonth.lengthOfMonth();	//method used to find the total amount of days in the record's month
+		boolean validRecordDate = true;	//is the record's date valid?
+		int yearDifference, monthDifference, dayDifference;	//variables for different in dates
+		//if record date is ahead of current date, print invalid date
+		if ((recordYear>=currentYear && recordMonth>currentMonth) || (recordYear>=currentYear && recordMonth>=currentMonth && recordDay>currentDay) || (recordYear > currentYear)) {
+			System.out.println("Invalid record date");
+			validRecordDate = false;
+		}
+		//if record's month is ahead of current month, add 12 to the current month and subtract 1 from the current year to compensate
+		if (currentMonth<recordMonth) {
+			currentMonth += 12;
+			currentYear-=1;
+		}
+		//if record's day of the month is ahead of current day, add the total days during the record's month to the amount in current days and subtract 1 month from current month to compensate
+		if (currentDay<recordDay){
+			currentDay+=days;
+			currentMonth-=1;
+		}
+		//if record's date is not invalid, do the computations and print out the age of record
+		if (validRecordDate!=false) {
+			yearDifference = currentYear - recordYear;
+			monthDifference = currentMonth - recordMonth;
+			dayDifference = currentDay - recordDay;
+			System.out.printf("The age of the record is %d years, %d months, and %d days old", yearDifference, monthDifference, dayDifference);
+			System.out.println();
+		}
 	}
 	//Method used to find current date in the form of year/month/day, then splitting year, month and day into separate values and inserting into an array
 	public static String [] findCurrentDate(){
 		Date currentDate = new Date();
-	  SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");	//Format the date to year/month/day
-	  String date = format.format(currentDate);
-	  String [] dateArray = date.split("/");	//Split the values of the date
-	  return dateArray;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");	//Format the date to year/month/day
+		String date = format.format(currentDate);
+		String [] dateArray = date.split("/");	//Split values of date
+		return dateArray;
 	}
 }
